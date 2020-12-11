6de7c4dd394a7b521fb80584c0cba929e3b1a99c
Antoine Bernardeau <antoine.bernardeau@superchausette.fr>
2020-12-11 02:57:04 +0100

    [2020/1] Add part 2

--- 2020/1/main.rs
+++ 2020/1/main.rs
@@ -29,8 +29,8 @@ where P: AsRef<Path>, {
 }
 
 fn part1(sorted_numbers: &Vec<i32>) {
-    for (first_idx, first_value) in sorted_numbers.iter().enumerate() {
-        for (second_idx, second_value) in sorted_numbers.iter().enumerate() {
+    'first: for (first_idx, first_value) in sorted_numbers.iter().enumerate() {
+        'second: for (second_idx, second_value) in sorted_numbers.iter().enumerate() {
             // prevent self match
             if first_idx == second_idx {
                 continue;
@@ -40,12 +40,42 @@ fn part1(sorted_numbers: &Vec<i32>) {
                 continue
             }
             if first_value + second_value > EXPECTED_VALUE {
-                break
+                break 'second;
             }
             if first_value + second_value < EXPECTED_VALUE {
                 continue
             }
-            println!("[PART1]: Value found: {} and {}, the result is: {}", first_value, second_value, first_value*second_value)
+            println!("[PART1]: Value found: {} and {}, the result is: {}", first_value, second_value, first_value*second_value);
+            break 'first;
+        }
+    }
+}
+
+
+fn part2(sorted_numbers: &Vec<i32>) {
+    'first: for (first_idx, first_value) in sorted_numbers.iter().enumerate() {
+        'second: for (second_idx, second_value) in sorted_numbers.iter().enumerate() {
+            'third: for (third_idx, third_value) in sorted_numbers.iter().enumerate() {
+                // prevent self match
+                if first_idx == second_idx || first_idx == third_idx || second_idx == third_idx {
+                    continue;
+                }
+                // Do not compute multiple time the same operation
+                if first_idx > second_idx {
+                    continue;
+                }
+                if first_value + second_value > EXPECTED_VALUE {
+                    break 'second;
+                }
+                if first_value + second_value + third_value > EXPECTED_VALUE {
+                    break 'third;
+                }
+                if first_value + second_value + third_value < EXPECTED_VALUE {
+                    continue;
+                }
+                println!("[PART2]: Value found: {}, {} and {}, the result is: {}", first_value, second_value, third_value, first_value*second_value*third_value);
+                break 'first;
+            }
         }
     }
 }
@@ -63,5 +93,6 @@ fn main() {
     let sorted_numbers = numbers;
     println!("{} Numbers found", sorted_numbers.len());
 
-    part1(&sorted_numbers)
+    part1(&sorted_numbers);
+    part2(&sorted_numbers);
 }
